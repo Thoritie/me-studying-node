82e495a8c546c480360430f15a553599244352ed
Thoritie <thohirh9@gmail.com>
2020-07-28 19:24:54 +0700

    :recycle: Refactor function to use es6 and arrow function :snowman:

--- notes-taking/notes.js
+++ notes-taking/notes.js
@@ -1,16 +1,11 @@
 const chalk = require('chalk')
 const fs = require('fs')
 
-const getNotes = function() {
-    return 'Your notes.....'
-}
+const getNotes = () => 'Your notes.....'
 
-
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes()
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title
-    })
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -24,12 +19,12 @@ const addNote = function (title, body) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes)
     fs.writeFileSync('notes.json', dataJSON)
 }
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json')
         const dataJSON = dataBuffer.toString()
@@ -39,11 +34,9 @@ const loadNotes = function () {
     }
 }
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes()
-    const noteToKeep = notes.filter(function (note) {
-        return note.title !== title
-    })
+    const noteToKeep = notes.filter((note) => note.title !== title)
 
     if (notes.length > noteToKeep.length ) {
         console.log(chalk.green.inverse('Note Remove!!'))
